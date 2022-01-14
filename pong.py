592f34f98690e266dc2158bc32df568dfb9307e5
Snerksss <berndts.tin21@student.dhbw-heidenheim.de>
2022-01-14 09:50:37 +0100

    update

--- pong.py
+++ pong.py
@@ -1,4 +1,6 @@
 from OutputFramework.OutFramework import OutputFramework as oF
+import numpy as np
 
-oF.setPixel(0,0,255,255,255)
+#oF.setPixel(0,0,255,255,255)
 #oF.showText("Hallo", 255, 255, 255, 12)
+oF.show(np.full((16,16,3),255))
